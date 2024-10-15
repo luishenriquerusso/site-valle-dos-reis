7e6c9b41dcc10cc1b998051fa74128fea4a8b9c4
Russo StnWeb <russo@stnweb.com.br>
2024-10-15 08:43:39 -0300

    never used ok

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,15 +1,15 @@
 'use client'
 
-import { useSession } from 'next-auth/react';
+//import { useSession } from 'next-auth/react';
 import { Logo } from "./logo";
 
-import { usePathname } from 'next/navigation';
+//import { usePathname } from 'next/navigation';
 
 export function Header(){
 
-  const { data: session } = useSession();
-  const user = session?.user;
-  const pathname = usePathname();
+  //const { data: session } = useSession();
+  // const user = session?.user;
+  // const pathname = usePathname();
 
   return(
     <div className=" sticky top-0 z-50 mt-4 container flex justify-center">
