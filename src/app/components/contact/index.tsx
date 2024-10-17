74c33259a644bac880bec6872671174ecee70e40
Russo StnWeb <russo@stnweb.com.br>
2024-10-17 07:59:24 -0300

    contato

---
+++ src/app/components/contact/index.tsx
@@ -0,0 +1,12 @@
+import { Map } from './map'
+import { Form } from './form'
+
+export function Contact(){
+    return(
+        <div>
+            <p>Contato</p>
+            <Map/>
+            <Form/>
+        </div>
+    )
+}
---
+++ src/app/components/contact/map.tsx
@@ -0,0 +1,7 @@
+export function Map(){
+    return(
+        <div>
+            <p>Map</p>
+        </div>
+    )
+}
