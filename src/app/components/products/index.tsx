d91859928c35400555049238b6c497546a410536
Russo StnWeb <russo@stnweb.com.br>
2024-10-22 09:15:26 -0300

    terminado de tipar

--- src/app/components/products/index.tsx
+++ src/app/components/products/index.tsx
@@ -4,13 +4,13 @@ import * as React from "react"
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
 import { ProductCard } from "./productCard"
 
-export function Products({ products }: {products: any[]}) {
+export function Products({ products }: {products: Product[]}) {
 
   return (
     <Carousel className="container ">
       
       <CarouselContent>
-        {products?.map((product : any, index: any) => (
+        {products?.map((product : Product) => (
           <CarouselItem key={product.id} className="md:basis-1/2 lg:basis-1/3 2xl:basis-1/4 p-4 md:p-8 max-w-80">            
             <ProductCard product={product} />
           </CarouselItem>
