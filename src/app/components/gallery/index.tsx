7ebba6db3377aad86a7bb00978f229ec72cf4099
Russo StnWeb <russo@stnweb.com.br>
2024-10-24 09:37:19 -0300

    subiu

--- src/app/components/gallery/index.tsx
+++ src/app/components/gallery/index.tsx
@@ -4,7 +4,7 @@ export function Gallery() {
   return (
     <div className="container w-full bg-slate-50 py-8 border-slate-200 border-b-2 my-10 mb-28">
 
-<div className="flex flex-col md:flex-row items-center justify-center md:space-x-8">
+    <div className="flex flex-col md:flex-row items-center justify-center md:space-x-8">
         <div className="mb-8">
           <h2 className="text-3xl font-bold text-gray-800 text-center">
             Cemitério Valle dos Reis
@@ -17,7 +17,8 @@ export function Gallery() {
           </p>
           <hr className="border-t-2 border-[#ae9668] w-1/4 mx-auto"/>
         </div>
-      </div>
+    </div>
+    
     <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
       {[1, 2, 3, 4, 5,6,7,8,9,10].map((id) => (
         <ImageGalleryItem key={id} id={1} />
